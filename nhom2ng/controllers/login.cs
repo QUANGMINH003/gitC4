f638da1cccb26057a7be4d3864ac5c3fbdd93eb0
nghiaph <phanhieunghia1054@gmail.com>
2023-12-20 14:08:52 +0700

    Create login.cs in folder controllers

---
+++ nhom2ng/controllers/login.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nhom2ng.controllers
+{
+    internal class login
+    {
+    }
+}
